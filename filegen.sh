227c899a66a1967227a832b91f8e9f5bd9afa06f
agrabarocherga <agrabarocherga@gmail.com>
2019-04-12 14:56:05 +0300

    Update filegen.sh

--- filegen.sh
+++ filegen.sh
@@ -1,43 +1,26 @@
-#!/bin/bash
-# filegen.sh
-# Генератор мусорных файлов фиксированного размера
-# Использование: filegen <file> 
-# Автор: agrabarocherga
-
-# проверка на уровень доступа
-#if [[ $UID -ne 0 ]]; then
-#  echo "ERROR: you must be root to run me. Get more privileges."
-#  exit 1
-#fi
-
-SIZE=10M 
-COUNT=1
-
-if [[ $# -ne 1 ]]; then
-  echo "Usage: $0 <file>"
-  exit 1
-fi
-
-if [[ !`touch $1` ]]; then
-  echo "ERROR: file $1 could not be created."
-  exit 1
-fi
-
-echo "Created file: $1"
-echo -e "Filling it with random bytes...\c"
-
-if [[ !-e /dev/urandom ]]; then
-	mknod -m 666 /dev/random c 1 8
-    mknod -m 666 /dev/urandom c 1 9
-    chown root:root /dev/random /dev/urandom
-fi
-
-if [[ !`dd if=/dev/urandom of=$1 bs=$SIZE count=$COUNT` ]]; then 
-  echo "ERROR: Something went wrong."
-  exit 1
-fi
-
-echo "done!"
-echo "File $1 created, size = `expr $SIZE * $COUNT` "
-
-exit 0
+#!/bin/bash                                                                                                                                                                                       
+# filegen.sh                                                                                                                                                                                      
+                                                                                                                                                                                                  
+SIZE=10                                                                                                                                                                                           
+COUNT=1                                                                                                                                                                                           
+                                                                                                                                                                                                  
+if [[ $# -ne 1 ]]; then                                                                                                                                                                           
+  echo "Usage: $0 <file>"                                                                                                                                                                         
+  exit 1                                                                                                                                                                                          
+fi                                                                                                                                                                                                
+                                                                                                                                                                                                  
+if [[ `touch $1` -ne 0 ]]; then                                                                                                                                                                   
+  echo "ERROR: file $1 could not be created."                                                                                                                                                     
+  exit 1                                                                                                                                                                                          
+fi                                                                                                                                                                                                
+                                                                                                                                                                                                  
+echo "Created file: $1"                                                                                                                                                                           
+echo -e "Filling it with random bytes...\c"                                                                                                                                                       
+                                                                                                                                                                                                  
+dd if=/dev/urandom of=$1 bs=${SIZE}M count=$COUNT status=none                                                                                                                                     
+                                                                                                                                                                                                  
+echo "done!"                                                                                                                                                                                      
+echo -e "File $1 created, size =\c"                                                                                                                                                               
+echo `expr $SIZE*$COUNT` MB                                                                                                                                                                       
+                                                                                                                                                                                                  
+exit 0                
